41dd2633ad23422a6e94bba75c11b50d85daaf38
Syed Asim Ali <s.asim.ali.se@gmail.com>
2018-11-04 23:55:59 +0500

    -Added Bottom Nav Bar dependency along with LifeCycle and legacy dependencies -Added Bottom Nav bar menu item -Added ToolBar -Added Bottom Nav Bar -Navigation Setup with Toolbar done -Navigation Setup with Bottom Nav Bar done

--- app/src/main/java/com/asimse/daggersampleapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/asimse/daggersampleapp/ui/main/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.asimse.daggersampleapp.ui.main
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.asimse.daggersampleapp.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupNavigation()
+    }
+
+    private fun setupNavigation(){
+
+        setSupportActionBar(toolbar)
+
+        navController = findNavController(R.id.navHostMain)
+
+        //ActionBar
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        //BottomNavBar
+        NavigationUI.setupWithNavController(bottomNavBar, navController)
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
